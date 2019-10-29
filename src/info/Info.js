12017dee14f12cd06ea6dbcf589e3ae45872924a
WuZekun <1430755343@qq.com>
2019-10-29 21:38:31 +0800

    new

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import './Info.css';
 import { Link } from 'react-router-dom'
-import backImg from './back.png'
 import detailImg from './detail.png'
 import girlImg from './girl.png'
+import logoutImg from './logout.png'
 
 class Info extends React.Component{
+
+  state = {
+    curTime: ''
+  }
+
+  componentDidMount() {
+    setInterval( () => {
+      this.setState({
+        curTime: new Date().toLocaleString()
+      })
+    }, 1000)
+  }
+
   render() {
     return (
       <div className="page">
-          <div className='header'>
-              <div className='back'>
-                  <img style={{ position: 'relative', height: '100%', width: '100%' }} src={backImg} alt='' />
+          <div className='headerInfo'>
+              <div className='out'>
+                  <img style={{ position: 'relative', height: '100%', width: '100%' }} src={logoutImg} alt='' />
                   <Link className='linkMask' to='../' />
               </div>
+              <div className='tips1'>退出登录</div>
+              <div className='welcome'>欢迎你，用户xxx</div>
+              <div className='welcome'>当前时间是：{this.state.curTime}</div>
           </div>
           <div className='detail'>
-            <p style={{paddingLeft: '20%'}}>当前温度：15 摄氏度</p>
-            <p style={{paddingLeft: '20%'}}>当前湿度：70%</p>
+            <div style={{paddingLeft: '50px'}}>当前温度：15 摄氏度</div>
+            <div style={{paddingLeft: '10%'}}>当前湿度：70%</div>
             <div className='jump' >
               <div>
                 <img style={{height: '50px', width: '50px'}} src={detailImg} alt='' />
